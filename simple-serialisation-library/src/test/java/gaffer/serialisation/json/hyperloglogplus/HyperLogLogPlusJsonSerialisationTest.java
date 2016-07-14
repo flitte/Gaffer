3fa9f6678b3ab9d529dd0a1a2a187c8283c07bcf
flitte <flitte@users.noreply.github.com>
2016-07-14 15:34:21 +0100

    Updated HyperLogLogPlusJsonSerialisationTest.java
    Allowed serialisation of default values.

--- simple-serialisation-library/src/test/java/gaffer/serialisation/json/hyperloglogplus/HyperLogLogPlusJsonSerialisationTest.java
+++ simple-serialisation-library/src/test/java/gaffer/serialisation/json/hyperloglogplus/HyperLogLogPlusJsonSerialisationTest.java
@@ -41,7 +41,6 @@ public class HyperLogLogPlusJsonSerialisationTest {
     public void before() {
         mapper = new ObjectMapper();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
 
         final SimpleModule module = new SimpleModule(HyperLogLogPlusJsonConstants.HYPER_LOG_LOG_PLUS_SERIALISER_MODULE_NAME, new Version(1, 0, 0, null));
         module.addSerializer(HyperLogLogPlus.class, new HyperLogLogPlusJsonSerialiser());
